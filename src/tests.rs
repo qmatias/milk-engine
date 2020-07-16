1849c34b8d724ae6205e28109fac7dccef87f1c6
Matias Kotlik <mdkotlik@gmail.com>
2020-07-15 23:46:45 -0500

    test /comments route

--- src/tests.rs
+++ src/tests.rs
@@ -1,4 +1,5 @@
 use super::rocket;
+use crate::build_rocket;
 use rocket::http::Status;
 use rocket::local::Client;
 
@@ -11,11 +12,12 @@ fn test_routes() {
         "/shop/cum",
         "/shop/parts",
         "/about",
+        "/comments",
     ]);
 }
 
 fn test_urls(urls: Vec<&str>) {
-    let mut client = Client::new(prepare()).unwrap();
+    let mut client = Client::new(build_rocket()).unwrap();
     for url in urls {
         test_url(&mut client, url)
     }
