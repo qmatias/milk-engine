9d1ea8b3ae999378f6171eb9238953fc8f4cffc7
qmatias <mdkotlik@gmail.com>
2021-10-16 16:09:59 +0000

    randomize sales and remove obselete images

--- src/home.rs
+++ src/home.rs
@@ -1,6 +1,7 @@
 use crate::Ads;
 use rocket::State;
 use rocket_contrib::templates::Template;
+use rand::prelude::*;
 
 #[derive(Serialize)]
 struct HomeContext<'r> {
@@ -8,6 +9,8 @@ struct HomeContext<'r> {
     desc: &'static str,
     image: &'static str,
     ads: &'r Vec<Ad>,
+	sale_image: &'static str,
+	sale_link: &'static str,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -20,6 +23,11 @@ pub struct Ad {
 
 #[get("/")]
 pub fn index(ads: State<Ads>) -> Template {
+	let sales = vec![
+		("sale-milk.jpg", "/shop/milk"),
+		("sale-engine.jpg", "/shop/engines")
+	];
+	let sale = sales.choose(&mut thread_rng()).unwrap();
     Template::render(
         "index",
         HomeContext {
@@ -27,6 +35,8 @@ pub fn index(ads: State<Ads>) -> Template {
             desc: "Milk Engineers - Home of the Milk Engine",
             image: "icon.png",
             ads: ads.inner(),
+			sale_image: sale.0,
+			sale_link: sale.1,
         },
     )
 }
